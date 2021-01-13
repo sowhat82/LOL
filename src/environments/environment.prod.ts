c4fd895b45876b4d4b13a0362c4b29d868862830
Alvin <alvincheong@u.nus.edu>
2021-01-13 20:40:47 +0800

    env prod file for chat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
+const protocol = window.location.protocol.replace('http', 'ws');
+const host = window.location.host;
+
 export const environment = {
   production: true,
-  baseUrl: '${protocol}//${host}'
+  baseUrl: `${protocol}//${host}`
 };
